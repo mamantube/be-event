76fcf2c18b168bb456121423d186c2e3999fdb39
mamantube <firmanhiday.id007@gmail.com>
2025-11-11 11:44:49 +0700

    implementation body-parser

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import router from "./routes/api";
+import bodyParser from "body-parser";
 
 const app = express();
+
+app.use(bodyParser.json())
 
 const PORT = 3001;
 
