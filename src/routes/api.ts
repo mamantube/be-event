c045010f2a3e6680f0e7082f428d091da0f119d3
mamantube <firmanhiday.id007@gmail.com>
2025-11-11 11:44:25 +0700

    tidy up

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,9 +1,7 @@
 import express from "express";
-import dummyController from "../controllers/dummy.cotroller"
 
 
 const router = express.Router();
 
-router.get("/dummy", dummyController.dummy);
 
 export default router;
