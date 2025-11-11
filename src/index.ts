fff9d6c8e2665957e32bcb12171ac86c223ad374
mamantube <firmanhiday.id007@gmail.com>
2025-11-11 18:37:27 +0700

    add body-parser end express json

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,11 @@ import bodyParser from "body-parser";
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }))
 
 const PORT = 3001;
 
