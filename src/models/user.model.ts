3d8e664c3656842946014dfc693c9a1b74702a71
mamantube <firmanhiday.id007@gmail.com>
2025-11-26 10:47:46 +0700

    add sending email function to aktivation account

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import { encrypt } from "../utils/encryption";
+import { renderMailHtml, ISendMail, sendMail } from "../utils/mail/mail"
+import { CLIENT_HOST, EMAIL_SMTP_USER } from "../utils/env";
 
 export interface User {
     full_name: string;
@@ -11,6 +13,7 @@ export interface User {
     profile_picture: string;
     is_active: boolean;
     activation_code: string;
+    created_at: string;
 }
 
 const Schema = mongoose.Schema;
@@ -63,6 +66,34 @@ UserSchema.pre("save", function (next) {
     const user = this;
     user.password = encrypt(user.password);
     next();
+});
+
+UserSchema.post("save", async function (doc, next) {
+    try {
+        const user = doc;
+        
+        console.log("Send mail to:", user.email);
+    
+        const contentEmail = await renderMailHtml("registration-success.ejs", {
+            user_name: user.user_name,
+            full_name: user.full_name,
+            email: user.email,
+            created_at: user.created_at,
+            activation_link: `${CLIENT_HOST}/auth/activation?code=${user.activation_code}`
+        });
+        
+        await sendMail({
+            from: EMAIL_SMTP_USER, 
+            to: user.email,
+            subject: "Aktivasi akun anda",
+            html: contentEmail
+        })
+    } catch (error) {
+        console.error(error)
+    } finally {
+        next();
+    }
+
 })
 
 UserSchema.methods.toJSON = function () {
