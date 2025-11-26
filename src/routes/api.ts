a0f704166bb5ee6f98ac3ac7a0e2ec5ef5918fc5
mamantube <firmanhiday.id007@gmail.com>
2025-11-26 15:38:44 +0700

    set up swagger to LoginRequest and RegisterRequest

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -5,8 +5,22 @@ import authMiddleware from "../middlewares/auth.middleware";
 
 const router = express.Router();
 
-router.post("/auth/register", authController.register);
-router.post("/auth/login", authController.login);
+router.post("/auth/register",
+    /**
+         #swagger.requestBody = {
+           required: true,
+           schema: {$ref: "#/components/schemas/RegisterRequest"}
+          }
+     */
+         authController.register);
+router.post("/auth/login",
+    /**
+         #swagger.requestBody = {
+           required: true,
+           schema: {$ref: "#/components/schemas/LoginRequest"}
+          }
+     */
+         authController.login);
 router.get("/auth/me", authMiddleware, authController.me);
 
 export default router;
