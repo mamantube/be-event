2643b6d21c874da9cf7e5eb444d2835dba5a91e4
mamantube <firmanhiday.id007@gmail.com>
2025-11-26 10:46:45 +0700

    nodemailer set up

---
+++ src/utils/mail/mail.ts
@@ -0,0 +1,41 @@
+import nodemailer from "nodemailer";
+import {EMAIL_SMTP_SERVICE_NAME, EMAIL_SMTP_PASSWORD, EMAIL_SMTP_PORT, EMAIL_SMTP_SECURE, EMAIL_SMTP_HOST, EMAIL_SMTP_USER} from "../env"
+import ejs from "ejs";
+import path from "path";
+
+const transporter = nodemailer.createTransport({
+    service: EMAIL_SMTP_SERVICE_NAME,
+    host: EMAIL_SMTP_HOST,
+    port: EMAIL_SMTP_PORT,
+    secure: EMAIL_SMTP_SECURE,
+    auth: {
+        user: EMAIL_SMTP_USER,
+        pass: EMAIL_SMTP_PASSWORD,
+    },
+    requireTLS: true
+});
+
+export interface ISendMail {
+    from: string;
+    to: string;
+    subject: string;
+    html: string;
+}
+
+export const sendMail = async({...mailParams}) => {
+    const resolve = await transporter.sendMail({
+       ...mailParams
+    })
+
+    return sendMail;
+}
+
+export const renderMailHtml = async (template: string, data: any): Promise<string> => {
+    const content = await ejs.renderFile(path.join(__dirname, 
+        // `templates/${template}`
+        "../mail/templates/regristration-success.ejs"
+    ), data)
+
+    return content as string;
+};
+
