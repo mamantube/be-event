c31f5142de2b6877b6a326491e71ab82d3865204
mamantube <firmanhiday.id007@gmail.com>
2025-11-26 16:40:12 +0700

    add swagget tags

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,26 +2,40 @@ import express from "express";
 import authController from "../controllers/auth.controller";
 import authMiddleware from "../middlewares/auth.middleware";
 
-
 const router = express.Router();
 
-router.post("/auth/register",
-    /**
+router.post(
+  "/auth/register",
+  /**
+        #swagger.tags = ['Auth']
          #swagger.requestBody = {
            required: true,
            schema: {$ref: "#/components/schemas/RegisterRequest"}
           }
      */
-         authController.register);
-router.post("/auth/login",
-    /**
-         #swagger.requestBody = {
-           required: true,
-           schema: {$ref: "#/components/schemas/LoginRequest"}
-          }
+  authController.register
+);
+router.post(
+  "/auth/login",
+  /** 
+        #swagger.tags = ['Auth']
+        #swagger.requestBody = {
+          required: true,
+          schema: {$ref: "#/components/schemas/LoginRequest"}
+        }
      */
-         authController.login);
+  authController.login
+);
 router.get("/auth/me", authMiddleware, authController.me);
-router.post("/auth/activatin", authController.activation);
+router.post(
+  "/auth/activatin",
+  /**
+          #swagger.tags = ['Auth']
+          #swagger.requestBody = {
+              required: true,
+              schema: {$ref: '#/components/schemas/ActivationRequest'}
+          }
+      */ authController.activation
+);
 
 export default router;
