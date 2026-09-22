79a5307c9e65b598b880960c7649bb7c46aaf360
mamantube <firmanhiday.id007@gmail.com>
2026-09-22 14:39:36 +0700

    update router

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 
 const PORT = 3000;
 
-app.use("/api", router);
+app.use("/api/v1", router);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
