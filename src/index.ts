16fb0114f1155f5d2fff351a814934f047e180eb
mamantube <firmanhiday.id007@gmail.com>
2026-09-22 15:21:13 +0700

    import body parser

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import router from "./routes/api";
+import bodyParser from "body-parser";
 
 const app = express();
-
 const PORT = 3000;
 
+app.use(bodyParser.json());
 app.use("/api/v1", router);
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
