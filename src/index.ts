5e1e1ba91ad9c15f79bb9beed28d420870c8a159
mamantube <firmanhiday.id007@gmail.com>
2026-09-22 17:22:45 +0700

    update code and connect db

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 import express from "express";
 import router from "./routes/api";
 import bodyParser from "body-parser";
+import db from "./utils/db";
 
-const app = express();
-const PORT = 3000;
+async function init() {
+  try {
+    const result = await db();
 
-app.use(bodyParser.json());
-app.use("/api/v1", router);
+    console.log("Database status:", result)
 
+    
+    const app = express();
+    const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-});
+    app.use(bodyParser.json());
+    app.use("/api/v1", router);
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+init();
