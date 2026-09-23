b281d40138d0995b0d279ae36105f3443cf07dd4
mamantube <firmanhiday.id007@gmail.com>
2026-09-23 16:12:43 +0700

    add secret key token

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,3 +3,4 @@ import dotenv from "dotenv";
 dotenv.config()
 
 export const DATABASE_URL: string = process.env.DATABASE_URL || "";
+export const SECRET_KEY_TOKEN: string = process.env.SECRET_KEY_TOKEN || "";
