60fb4288e1004e5a0463834687fcc95aa2acc991
mamantube <firmanhiday.id007@gmail.com>
2026-09-23 16:30:41 +0700

    update code

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,8 @@ async function init() {
 
     app.get("/", (req, res) => {
       res.status(200).json({
-        message: "Server is running"
+        message: "Server is running",
+        data: null
       })
     })
 
